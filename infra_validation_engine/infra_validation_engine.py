1c7d6baaea9800ef440b9ed8089ca921df786575
maany <imptodefeat@gmail.com>
2019-08-07 15:27:50 +0200

    include yaml compiler

--- infra_validation_engine/infra_validation_engine.py
+++ infra_validation_engine/infra_validation_engine.py
@@ -1,15 +1,16 @@
 import argparse
 import testinfra
-
+import simple_grid_yaml_compiler
+import yaml
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--filename')
-    parser.add_argument('--host')
+    parser.add_argument('--config_master_host')
     args = parser.parse_args()
     return {
         'filename': args.filename,
-        'host': args.host
+        'config_master_host': args.config_master_host
     }
 
 
@@ -23,10 +24,15 @@ def test_cm_git_install(cm):
 if __name__ == "__main__":
     args = parse_args()
     site_level_config_file = args['filename']
-    host = args['host']
-    if host is not None:
-        host = testinfra.get_host(host)
+    config_master_host = args['config_master_host']
+    if config_master_host is not None:
+        config_master_host = testinfra.get_host(config_master_host)
     else:
-        host = testinfra.get_host('local://')
-    output = test_cm_git_install(host)
-    print output
+        config_master_host = testinfra.get_host('local://')
+
+    output = test_cm_git_install(config_master_host)
+
+    simple_grid_yaml_compiler.execute_compiler(open(site_level_config_file, 'r'),
+                                               open('./.temp/augmented_site_level_config_file.yaml', 'w'),
+                                               './.temp/augmented_site_level_schema.yaml')
+    augmented_site_level_config = yaml.safe_load(open('./.temp/augmented_site_level_config_file.yaml', 'r'))
